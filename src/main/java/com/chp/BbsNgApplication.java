32ec2075d30cd66cf11127bfd7ca0e7cea88156e
chp <chenhp@visionet.com.cn>
2019-09-25 11:26:13 +0800

    初提交

--- src/main/java/com/chp/BbsNgApplication.java
+++ src/main/java/com/chp/BbsNgApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BbsNgApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BbsNgApplication.class, args);
     }
 
